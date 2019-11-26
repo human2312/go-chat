c2702883c0e34c264944e697840966ec6c14c96c
L.M.Y <human2312@qq.com>
2019-11-26 16:47:51 +0800

    一个长连接。。。。。

---
+++ src/main/main.go
@@ -0,0 +1,19 @@
+//main 入口
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"../socket"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/echo", socket.Echo)
+	http.HandleFunc("/websocket", socket.Home)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
---
+++ src/socket/socket.go
@@ -0,0 +1,39 @@
+package socket
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"html/template"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{} // use default options
+
+//输出
+func Echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+//首页
+func Home(w http.ResponseWriter, r *http.Request) {
+	homeTemplate, _ :=template.ParseFiles("../../web/index.html")
+	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
+}
