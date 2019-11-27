98c8d46b6fdacd207ea31462be7cf88016623ad9
L.M.Y <human2312@qq.com>
2019-11-27 22:04:56 +0800

    一个笨方法处理聊天室

--- src/socket/chat.go
+++ src/socket/chat.go
@@ -4,27 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type Message struct {
 	Username string
 	Message  string
-}
-
-type User struct {
-	Username string
-}
-
-type Datas struct {
-	Messages []Message
-	Users    []User
+	Lasttime int64
 }
 
 // 全局信息
-var datas Datas
-var users map[http.ResponseWriter]string
+var messages []Message
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	homeTemplate, _ := template.ParseFiles("../../web/chat.html")
@@ -36,15 +28,24 @@ func ChatSocket(w http.ResponseWriter, r *http.Request) {
 
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Print("upgrade:", err)
 		return
 	}
+
+	// 进入聊天室把历史聊天记录进行信息分发
+	for _, val := range messages {
+		s1 := []byte("{\"username\":\"" + val.Username + "\",\"message\":\"" + val.Message + "\",\"lasttime\":\"" + strconv.FormatInt(val.Lasttime, 10) + "\"}")
+		err = c.WriteMessage(1, s1)
+		if err != nil {
+			fmt.Println("发送出错: " + err.Error())
+			break
+		}
+	}
+
 	//defer c.Close()
 	for {
 		// 接收数据
 		mt, message, err := c.ReadMessage()
 		if err != nil {
-			log.Println("read:", err)
 			break
 		}
 
@@ -53,13 +54,26 @@ func ChatSocket(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			fmt.Println("解析数据异常")
 		}
+		fmt.Println(message)
+		//获取到前端最后阅读时间
+		lasttime := msg.Lasttime
 
-		log.Printf("recv: %s", msg.Username)
+		// 添加聊天记录到全局信息
+		msg.Lasttime = time.Now().Unix()
+		messages = append(messages, msg)
+
 		// 通过webSocket将当前信息分发
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			fmt.Println("发送出错: " + err.Error())
-			break
+		for _, val := range messages {
+			s1 := []byte("{\"username\":\"" + val.Username + "\",\"message\":\"" + val.Message + "\",\"lasttime\":\"" + strconv.FormatInt(val.Lasttime, 10) + "\"}")
+			fmt.Println(val)
+			fmt.Println(lasttime)
+			if val.Lasttime > lasttime {
+				err = c.WriteMessage(mt, s1)
+				if err != nil {
+					fmt.Println("发送出错: " + err.Error())
+					break
+				}
+			}
 		}
 
 	}
